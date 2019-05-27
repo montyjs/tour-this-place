ad22e53549f3f1c7429ea5d18927f894f880af6b
Chase Wood <wood.c.chase@gmail.com>
2019-05-26 18:24:13 -0600

    Finished main component minus minor styling tweaks

---
+++ client/src/components/PhotoRow.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled, {createGlobalStyle} from 'styled-components';
+
+
+const Image = styled.div`
+  background-image: ${props => `url('${props.photo}')`};
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  height: 100%;
+  width: 100%;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 200px;
+  text-align: left;
+`;
+
+const Room = styled.p`
+  font-size: 16px;
+  font-weight: 400;
+`;
+
+const Row = styled.p`
+  padding-top: 5rem;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const PhotoRow = ({photos}) => {
+  return (
+    <Row className="row pt-5 w-100">
+      {photos.map((photo, i) => {
+        return (
+	        <Container className="col-3 w-100">
+		        <Image photo={photo.photoUrl} className="img-fluid"/>
+		        <Room className="description">{photo.room}</Room>
+		      </Container>
+	      )
+        }
+      )}
+    </Row>
+  );
+};
+
+export default PhotoRow;
--- server/index.js
+++ server/index.js
@@ -2,15 +2,31 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 let app = express();
 
-
-app.use(express.static(__dirname + 'public'))
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors());
 
 app.get('/api/listings', (req, res) => {
 	database('listings')
-	  .then((listings) => res.status(200).send(listings))
+	  .then(listings => res.status(200).send(listings))
+})
+
+app.post('/api/photos', (req, res) => {
+	database('photos')
+	  .where({ listing_id: req.body.id })
+	  .then(photos => res.status(200).send(photos))
+})
+
+app.get('/api/photos', (req, res) => {
+	database('photos')
+	  .then(photos => res.status(200).send(photos))
 })
 
 app.listen(3001);
+console.log('server listening on port 3001');
