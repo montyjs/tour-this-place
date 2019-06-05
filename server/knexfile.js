58d43d842f2f56df28495230f9592729c12d925a
Chase Wood <wood.c.chase@gmail.com>
2019-06-04 23:33:47 -0600

    Refactored random photo seeding

--- server/knexfile.js
+++ server/knexfile.js
@@ -8,5 +8,13 @@ module.exports = {
       directory: __dirname + '/database/migrations',
       seeds: __dirname + '/seeds/seed.js'
     }
+  },
+  production: {
+    client: 'pg',
+    connection: 'http://airbnb-tour.c13ufv3uqugk.us-west-1.rds.amazonaws.com/',
+    migrations: {
+      directory: __dirname + '/database/migrations',
+      seeds: __dirname + '/seeds/seed.js'
+    }
   }
 };
--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,48 +1,100 @@
 /*________________________Building Data____________________________*/
-const faker = require('faker');
+const LIVING_ROOM = [
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Living+Room/img1.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Living+Room/img2.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Living+Room/img3.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Living+Room/img4.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Living+Room/img5.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Living+Room/img6.jpg'
+];
 
+const DINING_ROOM = [
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Dining+Room/img1.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Dining+Room/img2.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Dining+Room/img3.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Dining+Room/img4.jpg'
+];
 
-const rooms = ['Living room', 'Dining room', 'Bathroom', 'Bedroom', 'Kitchen', 'Loft', 'Garage', 'Garden', 'Man cave', 'Porch'];
-const types = ['Townhouse', 'Villa', 'Apartment', 'Mansion', 'Shack', 'Lean-to', 'Compound', 'Hostel'];
+const BATHROOM = [
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Bathroom/img1.jpg', 
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Bathroom/img3.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Bathroom/img5.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Bathroom/img6.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Bathroom/img7.jpg'
+];
 
+const BEDROOM = [
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Bedroom/img1.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Bedroom/img2.jpg', 
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Bedroom/img3.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Bedroom/img4.jpg', 
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Bedroom/img5.jpg'
+];
+
+const KITCHEN = [
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Kitchen/img1.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Kitchen/img2.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Kitchen/img4.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Kitchen/img5.jpg'
+];
+
+const PATIO = [
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Patio/img1.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Patio/img2.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Patio/img3.jpg'
+];
+
+const ENTRANCE = [
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Entrance/img1.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Entrance/img2.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Entrance/img3.jpg',
+  'https://airbnb-tour-photos.s3-us-west-1.amazonaws.com/room_photos/Entrance/img4.jpg'
+];
+
+const ROOMS = Promise.all([[LIVING_ROOM, 'Living Room'], [DINING_ROOM, 'Dining Room'], [BATHROOM, 'Bathroom'], [BEDROOM, 'Bedroom'], [KITCHEN, 'Kitchen'], [PATIO, 'Patio'], [ENTRANCE, 'Entrance']]);
+const TYPES = ['Townhouse', 'Villa', 'Apartment', 'Mansion', 'Shack', 'Lean-to', 'Compound', 'Hostel'];
 
 let seedListings = [];
 
-
 for(let i=0; i<100; i++) {
   let listing = {
-    type: types[Math.floor(Math.random()*(types.length-1))]
+    type: TYPES[Math.floor(Math.random()*(TYPES.length-1))]
   };
   seedListings.push(listing);
 }
 /*________________________Seeding Data____________________________*/
 exports.seed = function(knex, Promise) {
-  return knex('photos').del()
-    .then(() => knex('listings').del())
-    .then(() => {
-      return Promise.all(seedListings.map(listing => {
-        return knex('listings').insert(listing);
-      }))
-    })
-    .then(() => {
-      return knex('listings')
-        .pluck('id')
-        .then(ids=> {
-          let temp = [];
-          ids.map((id) => {
-            for(let i=0;i<20+Math.random()*30;i++) {
-              photo = {
-                photoUrl: faker.image.imageUrl(),
-                room: rooms[Math.floor(Math.random()*(rooms.length-1))],
-                listing_id: id
-              }
-              temp.push(photo)
-            }
-          })
-          return temp;
+  return ROOMS
+    .then(rooms => {
+      return knex('photos').del()
+        .then(() => knex('listings').del())
+        .then(() => {
+          return Promise.all(seedListings.map(listing => {
+            return knex('listings').insert(listing);
+          }))
         })
-    })
-    .then((photos) => Promise.all(photos.map(photo => {
-      if (photo.room) return knex('photos').insert(photo);
-    })))
+        .then(() => {
+          return knex('listings')
+            .pluck('id')
+            .then(ids=> {
+              let temp = [];
+              ids.map((id) => {
+                for(let i=0;i<20+Math.random()*30;i++) {
+                  let ranRoom = rooms[Math.floor(Math.random()*(rooms.length-1))]
+                  let url = ranRoom[0][Math.floor(Math.random()*(ranRoom[0].length-1))]
+                  photo = {
+                    photoUrl: url,
+                    room: ranRoom[1],
+                    listing_id: id
+                  }
+                  temp.push(photo)
+                }
+              })
+              return temp;
+            })
+        })
+        .then((photos) => Promise.all(photos.map(photo => {
+          if (photo.room) return knex('photos').insert(photo);
+        })))
+    });
 };
